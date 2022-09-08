e9629e32762c4a60e636a7ecf5ee79569a0ca87d
Nigmatulin Timur <timurnigmatulin6@gmail.com>
2022-09-08 21:22:03 +0500

    1.2, 1.3
    сумма элементов, произведение элементов

---
+++ lab1/ActionsWithElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public static class ActionsWithElements
+    {
+        /// <summary>
+        /// Возвращает сумму элементов последовательности
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <returns></returns>
+        public static long Sum(long[] vector)
+        {
+            long result = 0;
+            foreach (var item in vector)
+            {
+                result += item;
+            }
+            return result;
+        }
+        /// <summary>
+        /// Возвращает произведение элементов последовательности
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <returns></returns>
+        public static long Mul(long[] vector)
+        {
+            long result = 1;
+            foreach (var item in vector)
+            {
+                result *= item;
+            }
+            return result;
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+class Program
+{ 
+    static void Main()
+    {
+        var timer = new Stopwatch();
+        var rnd = new Random();
+        long result = 0;
+        Console.WriteLine("Введите длину вектора V");
+        var N = int.Parse(Console.ReadLine());
+        long[] vector = new long[N];
+        for (int i = 0; i < N; i++)
+        {
+            vector[i] = rnd.Next(1, 20);
+        }
+        /*foreach (int i in vector)
+            Console.Write($"{i} ");*/
+        List<string> milliSec = new();
+        for (int i = 0; i < 5; i++)
+        {
+            
+            timer.Start();
+            result = lab1.ActionsWithElements.Sum(vector);
+            timer.Stop();
+            milliSec.Add(timer.ElapsedTicks.ToString());
+            timer.Reset();
+
+        }
+        milliSec.Add("результат вычисления: " + result.ToString());
+        milliSec.Add("количество итераций: " + N.ToString());
+
+        File.WriteAllLines("..//..//..//1_2_result.txt", milliSec);
+        milliSec.Clear();
+
+        for (int i = 0; i < 5; i++)
+        {
+
+            timer.Start();
+            result = lab1.ActionsWithElements.Mul(vector);
+            timer.Stop();
+            milliSec.Add(timer.ElapsedTicks.ToString());
+            timer.Reset();
+
+        }
+        milliSec.Add("результат вычисления: " + result.ToString());
+        milliSec.Add("количество итераций: " + N.ToString());
+
+        File.WriteAllLines("..//..//..//1_3_result.txt", milliSec);
+
+        //Console.WriteLine(milliSec);
+    }
+}
