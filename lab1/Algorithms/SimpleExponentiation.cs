97678c7a6de7580c25e93ec146a89b14f7fcdbe7
Ilya Kvashnin <27romashka05@gmail.com>
2022-09-15 23:52:38 +0500

    Добавил реализацию простого алгоритма возведения в степень

---
+++ lab1/Algorithms/SimpleExponentiation.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace lab1.Algorithms;
+
+public class SimpleExponentiation
+{
+    private static int countSteps = 0;
+    /// <summary>
+/// Простой алгоритм возведения в степень. 
+/// </summary>
+/// <param name="value">Значение, которое возводится в степень</param>
+/// <param name="pow">Степень, в которую нужно возвести значение</param>
+/// <returns></returns>
+    public static (string Pow,string Count) PowFunc(ulong value, int pow)
+    {    
+        countSteps += 1;
+        BigInteger result;
+        countSteps += 1;
+        result = value;
+        for (int i = 1; i < pow; i++)
+        {
+            countSteps += 1;
+            countSteps += 1;
+            result *= value;
+        }
+        return (pow.ToString(),countSteps.ToString());
+    }
+}
--- lab1/Program.cs
+++ lab1/Program.cs
@@ -6,50 +6,22 @@ class Program
 { 
     static void Main()
     {
-        /*var timer = new Stopwatch();
-        var vector = VectorGeneration.GetNewVector(100, 1, 9);
-        List<string> milliSec = new();
-        Console.WriteLine("Введите ранг матрицы: ");
-        var N = int.Parse(Console.ReadLine());
-        
-       
-        for (int i = 0; i < 5; i++)
+        List<string> outputData = new();
+        outputData.Add(new string("Pow" + "\t" + "Count steps"));
+       (string Pow, string Count) result = new();
+        for (int i = 1; i <= 50; i += 10)
         {
-            
-            timer.Start();
-            // тута должен быть алгоритм умножения матриц
-            timer.Stop();
-            milliSec.Add(timer.ElapsedMilliseconds.ToString());
-            timer.Reset();
-
-        }
-        File.WriteAllLines("..//..//..//2_result.txt", milliSec);*/
-
-        var timer = new Stopwatch();
-        long result = 0;
-        
-
-        List<string> milliSec = new();
-        for (int j = 1; j <= 5; j++)
-        {
-            for (int i = 200; i < 10000; i+=25)
+            if (i == 11)
             {
-                int[] vector = VectorGeneration.GetNewVector(i, 1, 9);
-                timer.Start();
-                //VectorOperations.Const(); //1.1
-                //result = VectorOperations.Sum(vector); //1.2
-                result = VectorOperations.Mul(vector); //1.3
-                timer.Stop();
-                milliSec.Add(timer.Elapsed.TotalMilliseconds.ToString());
-                timer.Reset();
-
+                i -= 1;
+               result = SimpleExponentiation.PowFunc(5, i);
             }
-            //File.WriteAllLines($"..//..//..//results/1.1/1_1_{j}_result.txt", milliSec); //1.1
-            //File.WriteAllLines($"..//..//..//results/1.2/1_2_{j}_result.txt", milliSec); //1.2
-            File.WriteAllLines($"..//..//..//results/1.3/1_3_{j}_result.txt", milliSec); //1.3
-            
-            milliSec.Clear();
+            else
+            {
+               result = SimpleExponentiation.PowFunc(5, i);
+            }
+            outputData.Add(result.Pow + "\t" + result.Count);
         }
-       
+        File.WriteAllLines("..//..//..//results/8/8.1_result.txt", outputData);  
     }
 }
