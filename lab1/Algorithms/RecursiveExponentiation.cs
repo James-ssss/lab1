28a38fc191a2098efdaa69a3df5dbfb7f4029b57
Ilya Kvashnin <27romashka05@gmail.com>
2022-09-17 00:07:07 +0500

    Сделан алгоритм 2.2

--- lab1/Algorithms/RecursiveExponentiation.cs
+++ lab1/Algorithms/RecursiveExponentiation.cs
@@ -9,24 +9,24 @@ namespace lab1.Algorithms
 {
     public class RecursiveExponentiation
     {
-        static int countSteps = 0;
+        public static int CountSteps = 0;
         /// <summary>
-     /// Рекурсивный алгоритм возведения числа в степень.
-     /// </summary>
-     /// <param name="value">Значение, которое возводится в степень.</param>
-     /// <param name="pow">Степень, в которую нужно возвести значение.</param>
-     /// <returns>Возвращается первым степень, в которую возводится число. Вторым количество шагов, которое проходится алгоритмом./returns>
-        public static (int Pow, int Count, BigInteger Value) Exponentiate(int value, int pow)
+        /// Рекурсивный алгоритм возведения числа в степень.
+        /// </summary>
+        /// <param name="value">Значение, которое возводится в степень.</param>
+        /// <param name="pow">Степень, в которую нужно возвести значение.</param>
+        /// <returns>Возвращается первым степень, в которую возводится число. Вторым количество шагов, которое проходится алгоритмом./returns>
+        public static (int pow, BigInteger value) Exponentiate(int value, int pow)
         {
             if (pow == 0)
             {
-               countSteps += 2;
-               return (pow,countSteps, 1);
+                CountSteps += 2;
+                return (pow, 1);
             }
             if (pow > 0)
             {
-                countSteps += 2;
-                return (pow, countSteps, Exponentiate(value, pow - 1).Value * value);
+                CountSteps += 5;
+                return (pow, Exponentiate(value, pow - 1).value * value);
             }
             return Exponentiate(value, -pow);
         }
--- lab1/Program.cs
+++ lab1/Program.cs
@@ -9,12 +9,13 @@ class Program
         List<string> outputData = new();
         outputData.Add(new string("Pow" + "\t" + "Count steps"));
 
-        (int Pow, int Count, BigInteger Value) result = new();
+        (int Pow, BigInteger Value) result = new();
 
         for (int i = 0; i <= 100; i += 5)
         {
             result = RecursiveExponentiation.Exponentiate(2, i);
-            outputData.Add(result.Pow.ToString() + "\t" + result.Count.ToString());
+            outputData.Add(result.Pow.ToString() + "\t" + RecursiveExponentiation.CountSteps.ToString());
+            RecursiveExponentiation.CountSteps = 0;
         }
 
        File.WriteAllLines("..//..//..//results/8/8.2_result.txt", outputData);
